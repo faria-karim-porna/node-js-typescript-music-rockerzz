2d6d9fc88980b3393abd07c69bc8ea8d5d86895f
faria-karim-porna <faria.porna.08@gmail.com>
2022-05-01 08:52:43 +0000

    Audio File Uploader Added

--- build/app.js
+++ build/app.js
@@ -7,25 +7,53 @@ const express_1 = __importDefault(require("express"));
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = "mongodb+srv://dbUser:8NDok8kAl1aFKBAx@cluster0.7phu3.mongodb.net/PracticeDB?retryWrites=true&w=majority";
+const fileUpload = require("express-fileupload");
+const app = (0, express_1.default)();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(fileUpload());
+const port = 5000;
+const uri = "mongodb+srv://dbUser:JGbWQtUaES8sUQ5X@cluster0.7phu3.mongodb.net/RockerzzDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
-const app = (0, express_1.default)();
-app.use(bodyParser.json());
-app.use(cors());
-const port = 5000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 client.connect((err) => {
-    const collection = client.db("PracticeDB").collection("Practice");
-    app.post("/add", (req, res) => {
-        const newAdd = req.body;
-        collection.insertOne(newAdd).then((result) => {
-            console.log(result);
+    const audiosCollection = client.db("RockerzzDB").collection("Audios");
+    console.log("database");
+    // const usersCollection = client.db("creativeAgency").collection("users");
+    // const adminsCollection = client.db("creativeAgency").collection("admins");
+    // const reviewsCollection = client.db("creativeAgency").collection("reviews");
+    app.post("/uploadAudio", (req, res) => {
+        const file = req.files.file;
+        // const serviceTitle = req.body.serviceTitle;
+        // const serviceDescription = req.body.serviceDescription;
+        // console.log(file);
+        const imageList = [];
+        const musicList = [];
+        // const serviceTitle = req.body.serviceTitle;
+        // const serviceDescription = req.body.serviceDescription;
+        for (let index = 0; index < file.length; index++) {
+            const newFile = file[index].data;
+            const encFile = newFile.toString("base64");
+            const fileObj = {
+                contentType: file[index].mimetype,
+                size: file[index].size,
+                img: Buffer.from(encFile, "base64"),
+            };
+            if (file[index].mimetype === "image/jpeg") {
+                imageList.push(fileObj);
+            }
+            else {
+                musicList.push(fileObj);
+            }
+        }
+        audiosCollection.insertOne({ imageList, musicList }).then((result) => {
+            res.send(result.insertedCount > 0);
         });
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,61 @@ import express, { Application, Request, Response } from "express";
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const fileUpload = require("express-fileupload");
+
+const app: Application = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(fileUpload());
+const port: number = 5000;
+
 const uri =
-  "mongodb+srv://dbUser:8NDok8kAl1aFKBAx@cluster0.7phu3.mongodb.net/PracticeDB?retryWrites=true&w=majority";
+  "mongodb+srv://dbUser:JGbWQtUaES8sUQ5X@cluster0.7phu3.mongodb.net/RockerzzDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-const app: Application = express();
-app.use(bodyParser.json());
-app.use(cors());
-const port: number = 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
 client.connect((err: any) => {
-  const collection = client.db("PracticeDB").collection("Practice");
-  app.post("/add", (req, res) => {
-    const newAdd = req.body;
-    collection.insertOne(newAdd).then((result: any) => {
-      console.log(result);
+  const audiosCollection = client.db("RockerzzDB").collection("Audios");
+  console.log("database");
+  // const usersCollection = client.db("creativeAgency").collection("users");
+  // const adminsCollection = client.db("creativeAgency").collection("admins");
+  // const reviewsCollection = client.db("creativeAgency").collection("reviews");
+
+  app.post("/uploadAudio", (req: any, res: Response) => {
+    const file = req.files.file;
+    // const serviceTitle = req.body.serviceTitle;
+    // const serviceDescription = req.body.serviceDescription;
+    // console.log(file);
+    const imageList = [];
+    const musicList = [];
+    // const serviceTitle = req.body.serviceTitle;
+    // const serviceDescription = req.body.serviceDescription;
+    for (let index = 0; index < file.length; index++) {
+      const newFile = file[index].data;
+      const encFile = newFile.toString("base64");
+      const fileObj = {
+        contentType: file[index].mimetype,
+        size: file[index].size,
+        img: Buffer.from(encFile, "base64"),
+      };
+      if (file[index].mimetype === "image/jpeg") {
+        imageList.push(fileObj);
+      } else {
+        musicList.push(fileObj);
+      }
+    }
+
+    audiosCollection.insertOne({ imageList, musicList }).then((result: any) => {
+      res.send(result.insertedCount > 0);
     });
   });
-
 });
 
 app.listen(port, () => {
