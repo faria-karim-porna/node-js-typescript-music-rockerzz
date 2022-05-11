1ad6c510fe3a9f0e89ed661602d561a99e70347a
faria-karim-porna <faria.porna.08@gmail.com>
2022-05-11 04:26:35 +0000

    .env added

--- build/app.js
+++ build/app.js
@@ -9,39 +9,39 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const fileUpload = require("express-fileupload");
 const fs = require("fs");
-require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
+const app = (0, express_1.default)();
+require("dotenv").config();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(fileUpload());
+const port = 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qs1yz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 cloudinary.config({
     cloud_name: "derj0f1pt",
     api_key: "571272884278399",
     api_secret: "q4DwAlyepHvpv4Sv7kgG0FhIo7Q",
 });
 console.log("cloud_name", cloudinary.config().cloud_name);
-cloudinary.uploader
-    .upload(`${__dirname}/audios/3 Idiots/Aal Izz Well  3 Idiots  Aamir Khan Madhavan Sharman J  Sonu N Swanand K  Shaan  Shantanu M.mp3`, { public_id: "audios/3 Idiots/Aal Izz Well", resource_type: "auto" })
-    .then((result) => {
-    console.log("Success", JSON.stringify(result, null, 2));
-})
-    .catch((error) => {
-    console.log("error", JSON.stringify(error, null, 2));
-});
-const app = (0, express_1.default)();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(fileUpload());
-const port = 5000;
-const uri = "mongodb+srv://dbUser:JGbWQtUaES8sUQ5X@cluster0.7phu3.mongodb.net/RockerzzDB?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverApi: ServerApiVersion.v1,
-});
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 client.connect((err) => {
     const audiosCollection = client.db("RockerzzDB").collection("Audios");
     console.log("database");
+    cloudinary.uploader
+        .upload(`${__dirname}/audios/3 Idiots/Aal Izz Well  3 Idiots  Aamir Khan Madhavan Sharman J  Sonu N Swanand K  Shaan  Shantanu M.mp3`, { public_id: "audios/3 Idiots/Aal Izz Well", resource_type: "auto" })
+        .then((result) => {
+        console.log("Success", JSON.stringify(result, null, 2));
+    })
+        .catch((error) => {
+        console.log("error", JSON.stringify(error, null, 2));
+    });
     // const usersCollection = client.db("creativeAgency").collection("users");
     // const adminsCollection = client.db("creativeAgency").collection("admins");
     // const reviewsCollection = client.db("creativeAgency").collection("reviews");
--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,22 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const fileUpload = require("express-fileupload");
 const fs = require("fs");
-
+const cloudinary = require("cloudinary").v2;
+const app: Application = express();
 require("dotenv").config();
 
-const cloudinary = require("cloudinary").v2;
+app.use(bodyParser.json());
+app.use(cors());
+app.use(fileUpload());
+const port: number = 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qs1yz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 cloudinary.config({
   cloud_name: "derj0f1pt",
@@ -16,32 +28,6 @@ cloudinary.config({
 });
 console.log("cloud_name", cloudinary.config().cloud_name);
 
-cloudinary.uploader
-  .upload(
-    `${__dirname}/audios/3 Idiots/Aal Izz Well  3 Idiots  Aamir Khan Madhavan Sharman J  Sonu N Swanand K  Shaan  Shantanu M.mp3`,
-    { public_id: "audios/3 Idiots/Aal Izz Well", resource_type: "auto" }
-  )
-  .then((result: any) => {
-    console.log("Success", JSON.stringify(result, null, 2));
-  })
-  .catch((error: any) => {
-    console.log("error", JSON.stringify(error, null, 2));
-  });
-
-const app: Application = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(fileUpload());
-const port: number = 5000;
-
-const uri =
-  "mongodb+srv://dbUser:JGbWQtUaES8sUQ5X@cluster0.7phu3.mongodb.net/RockerzzDB?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
@@ -49,6 +35,17 @@ app.get("/", (req: Request, res: Response) => {
 client.connect((err: any) => {
   const audiosCollection = client.db("RockerzzDB").collection("Audios");
   console.log("database");
+  cloudinary.uploader
+    .upload(
+      `${__dirname}/audios/3 Idiots/Aal Izz Well  3 Idiots  Aamir Khan Madhavan Sharman J  Sonu N Swanand K  Shaan  Shantanu M.mp3`,
+      { public_id: "audios/3 Idiots/Aal Izz Well", resource_type: "auto" }
+    )
+    .then((result: any) => {
+      console.log("Success", JSON.stringify(result, null, 2));
+    })
+    .catch((error: any) => {
+      console.log("error", JSON.stringify(error, null, 2));
+    });
   // const usersCollection = client.db("creativeAgency").collection("users");
   // const adminsCollection = client.db("creativeAgency").collection("admins");
   // const reviewsCollection = client.db("creativeAgency").collection("reviews");
